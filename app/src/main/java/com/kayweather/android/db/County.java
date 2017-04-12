015b57e8ea7256a698c27ffc6d43cb2a4e6ad9f0
Kay <841750586@qq.com>
2017-04-12 12:09:27 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/kayweather/android/db/County.java
@@ -0,0 +1,45 @@
+package com.kayweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Kay on 2017/4/11.
+ */
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
